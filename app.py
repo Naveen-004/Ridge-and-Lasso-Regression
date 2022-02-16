a44602371833cdf9c8defffa986b42598aa963bb
Naveen-004 <chinna.nk4@gmail.com>
2022-02-16 19:45:06 +0530

    Implemented Cache memory for more functions.

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+# Importing required libraries
+import re
 import numpy as np
 import pandas as pd
 import seaborn as sns
@@ -11,18 +13,21 @@ from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import cross_val_score
 from sklearn.model_selection import train_test_split
 
+# Title for the Web App
 st.title('Ridge and Lasso Regression')
-        
+# Markdown for the description      
 st.markdown('''
 <h2>Explore Regularization</h2>
 <h6>Which one is the best Regularization for a particular dataset(sklearn datasets)?\n
 <ol><b><li>Ridge</li><li>Lasso</li></b></ol>''', unsafe_allow_html=True)
 
+# Dataset selection
 dataset_name = st.selectbox('Select Dataset',("Boston","Iris","Breast Cancer","Diabetes","Digits","Wine"))
 st.write('You Selected' + ' ' + dataset_name + ' ' +"Dataset" )
 
-@st.cache
-def get_dataset(dataset_name):
+# Function to get Features and Target --> X and Y
+@st.cache # Decorator to cache the function
+def get_dataset(dataset_name): # if-elif-else statement to get X and Y
     if dataset_name == 'Boston':
         df = datasets.load_boston()
         dataset = pd.DataFrame(df.data)
@@ -67,63 +72,70 @@ def get_dataset(dataset_name):
         y = dataset.iloc[:,-1]           
     return X, y
 
+# Getting X and Y
 X, y = get_dataset(dataset_name)
 st.write("Shape of Features :", X.shape)
 st.write('Overview of Features -', X)
 st.write("Shape of Target :", y.shape)
 st.write('Overview of Target data -', y)
-
+# Description of the dataset
 st.write('''As Ridge and Lasso Regression models are how of regularizing the linear models so we first got to prepare a linear model. So now, let’s code for preparing a linear regression model''')
 
+# Heading for the Linear Regression
 st.write('# Linear Model\'s MSE')
-
-reg = LinearRegression()
+reg = LinearRegression() # Initializing the Linear Regression
+# neg_mean_squared_error function to calculate the MSE
 mse = cross_val_score(reg, X, y, scoring='neg_mean_squared_error',cv=5)
 mean_mse = np.mean(mse)
-
 st.write("Mean MSE of "+ dataset_name + ' ' +'Dataset is '+ ' ' +str(mean_mse))
-
+# split the data into train and test
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=0)
 
+# Heading for the Ridge Regression
 st.write('# Ridge Regression')
+@st.cache # Decorator to cache the function
+def ridge():
+    ridge = Ridge() # Initializing the Ridge Regression
+    alpha_val = np.array(range(-999,999))
+    # GridSearchCV to find the best parameters
+    params = {'alpha': alpha_val}
+    ridge_reg = GridSearchCV(ridge, params, scoring='neg_mean_squared_error',cv=5)
+    ridge_reg.fit(X,y) # Fitting the data
+    return ridge_reg
 
-ridge = Ridge()
-params = {'alpha':[1e-15,1e-10,1e-8,1e-3,1e-2,1,5,10,20,30,35,40,45,50,55,100]}
-ridge_reg = GridSearchCV(ridge, params, scoring='neg_mean_squared_error',cv=5)
-ridge_reg.fit(X,y)
-
-st.write("Best parameters : ", str(ridge_reg.best_params_))
-st.write('Best Score : ', str(ridge_reg.best_score_))
-ridge_score = ridge_reg.best_score_
-ridge_val = str(ridge_reg.best_params_)
-pred_ridge = ridge_reg.predict(X_test)
-fig = plt.figure()
+st.write("Best parameters : ", str(ridge().best_params_))
+st.write('Best Score : ', str(ridge().best_score_))
+ridge_score = ridge().best_score_
+ridge_val = str(ridge().best_params_)
+pred_ridge = ridge().predict(X_test)
+fig = plt.figure() # Initializing the figure
 sns.distplot(y_test-pred_ridge, color='blue')
-st.pyplot(fig)
+st.pyplot(fig) # plotting the figure
 
-
+# Heading for the Lasso Regression
 st.write('# Lasso Regression')
+@st.cache # Decorator to cache the function
+def lasso():
+    lasso = Lasso() # initializing the Lasso Regression
+    alpha_val = np.array(range(-999,999))
+    # GridSearchCV to find the best parameters
+    params = {'alpha': alpha_val}
+    lasso_reg = GridSearchCV(lasso, params, scoring='neg_mean_squared_error',cv=5)
+    lasso_reg.fit(X,y) # Fitting the data
+    return lasso_reg
 
-lasso = Lasso()
-params = {'alpha':[1e-15,1e-10,1e-8,1e-3,1e-2,1,5,10,20,30,35,40,45,50,55,100]}
-lasso_reg = GridSearchCV(lasso, params, scoring='neg_mean_squared_error',cv=5)
-lasso_reg.fit(X,y)
-
-st.write("Best parameters : ", str(lasso_reg.best_params_))
-st.write('Best Score : ', str(lasso_reg.best_score_))
-lasso_score = lasso_reg.best_score_
-lasso_val = str(lasso_reg.best_params_)
-
-pred_lasso = lasso_reg.predict(X_test)
-
-
-fig = plt.figure()
+st.write("Best parameters : ", str(lasso().best_params_))
+st.write('Best Score : ', str(lasso().best_score_))
+lasso_score = lasso().best_score_
+lasso_val = str(lasso().best_params_)
+pred_lasso = lasso().predict(X_test)
+fig = plt.figure() # initializing the figure
 sns.distplot(y_test-pred_lasso, color='blue')
-st.pyplot(fig)
+st.pyplot(fig) # plotting the distribution of the residuals
 
-
+# Conclusion
 st.write('# Conclusion')
-
+# if-else statement to get the best model
 if lasso_score < ridge_score:
     st.write('Implementation of Ridge and Lasso regularization is done. In this case, the Ridge is that the best method of adjustment, with a regularization value of ',ridge_val)
 else:
